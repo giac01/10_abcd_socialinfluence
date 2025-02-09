3d2e40e65d8a59d53971f5b490b78e23b7c2edc3
giac01 <bignardig@outlook.com>
2025-02-09 19:06:14 +0000

    m1b hmc pameter changes

--- 5_modelfitter_local.R
+++ 5_modelfitter_local.R
@@ -4,14 +4,14 @@ Sys.setenv(REMOVE_PARTICIPANTS_FEW_TRIALS = TRUE)
 Sys.setenv(VARIATIONAL = FALSE)
 Sys.setenv(SMALL_SAMPLE = FALSE)
 Sys.setenv(MYSEED = 2)
-Sys.setenv(WARMUP = 1200)
-Sys.setenv(ITER = 2400)
-Sys.setenv(CHAINS = 2)
-Sys.setenv(ADAPTDELTA = .95)
+Sys.setenv(WARMUP = 1500)
+Sys.setenv(ITER = 2500)
+Sys.setenv(CHAINS = 4)
+Sys.setenv(ADAPTDELTA = .975)
 
 
 source("2_load_social_influence_data.R")
 
 
-source(file.path("model_fitting_scripts","model1_d.R")) # seed 1 = slow second chain! 
+source(file.path("model_fitting_scripts","model1_d.R")) 
 
--- model_fitting_scripts/model1_d.R
+++ model_fitting_scripts/model1_d.R
@@ -10,14 +10,26 @@ modelarg =  c(
       )
     ),
     # sample_prior = "only",
-    brm_args
+    brm_args,
+    max_treedepth = 15,
+    init = function() {
+      list(
+        b = c(1.0,  # sit_values_initialrating2 (based on your posterior)
+              0.42,  # delta_pos
+              0.34), # delta_neg
+        sd_subject__delta_pos = 0.3,  # Group-level SD for delta_pos
+        sd_subject__delta_neg = 0.3,  # Group-level SD for delta_neg
+        cor_subject__delta_pos__delta_neg = .7,  # Start at 0 for better mixing
+        sigma = 1.1  # Start near estimated sigma
+      )
+    }
  )
   
 model1 = do.call(brm, modelarg)
   
 saveRDS(model1, file = paste0("/rds/user/gb424/hpc-work/model1",Sys.time(),".Rds"))
 
-#saveRDS(model1, file = file.path("saved_models",paste0("model1_c",Sys.time(),".Rds")))
+#saveRDS(model1, file = file.path("saved_models",paste0("model1_d",Sys.time(),".Rds")))
   
 
 
